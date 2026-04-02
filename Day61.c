928805bb651ee1e4ceb82c35b539fd9ca9b30ef0
bhawana2007-ui <bhawana2007.7@gmail.com>
2026-04-02 23:52:12 +0530

    Graph Representation Using Adjacency Matrix

---
+++ Day61.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    // Input number of vertices and edges
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    int adj[n][n];
+
+    // Initialize matrix with 0
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    int u, v;
+    
+    // Input edges
+    printf("Enter edges (u v):\n");
+    for(int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        
+        // Add edge (directed by default)
+        adj[u][v] = 1;
+
+        // Uncomment below for undirected graph
+        // adj[v][u] = 1;
+    }
+
+    // Print adjacency matrix
+    printf("\nAdjacency Matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
